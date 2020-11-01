2928bd5352472d6b7d94505e88135643b66ba8ae
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-11-01 20:23:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ let element = document.getElementById("main");
 element.remove();
 
 // Creates "h1#victory"
-let newHeader = document.createElement("h1#victory");
+document.createElement("h1#victory");
+
+// Gets "h1#victory"
+let newHeader = document.querySelector("")
